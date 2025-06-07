3bd455c4f1a309501a25035660fa7cdc0a5f4483
LucianaiB <lwc2023nl@gmail.com>
2025-06-08 00:44:01 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/148498470",
+    "https://blog.csdn.net/lwcwam/article/details/148502690",
     "https://blog.csdn.net/lwcwam/article/details/148498470",
     "https://blog.csdn.net/lwcwam/article/details/148197901"
 ]
